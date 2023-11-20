e21a14f77d231d8f63c582820222ebe164376548
Gabriel Valencia <jrjr75169@gmail.com>
2023-11-19 23:50:47 -0800

    finshed the api and starting the css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,29 @@
-// const apiKey = 'dd1d9de2bd9e57c7a7c9e5d7f23e13cf';
+const apiKey = 'dd1d9de2bd9e57c7a7c9e5d7f23e13cf';
 
 const input = document.querySelector('.search');
 const search = document.querySelector('.btn');
+const temp = document.querySelector('.temperature');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity');
+const cityName = document.querySelector('.name');
 
 
 
 search.addEventListener('click', function() {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input.value}&limit=1&appid=${apiKey}`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${input.value}&units=imperial&limit=1&appid=${apiKey}`)
     .then(Response => Response.json())
     .then(data => {
-        const lat = data[0].lat;
-        const lon = data[0].lon;
-        
-        weather(lat, lon);
-        forecast(lat, lon);
-    })
+        console.log(data);
+        const cityValue = data['name'];
+        const tempValue = data['main']['temp'];
+        const windvalue = data['wind']['speed'];
+        const humidityValue = data['main']['humidity'];
 
-    console.log(input.value);
+        cityName.innerHTML = cityValue;
+        temp.innerHTML = tempValue;
+        wind.innerHTML = windvalue;
+        humidity.innerHTML = humidityValue;
+    })
    
 })
 
